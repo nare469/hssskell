2aa07b303a6a11160317062c088cbb310abf96f6
nare469 <nare469@gmail.com>
2015-09-01 23:09:23 -0400

    Shifted origin

--- Main.hs
+++ Main.hs
@@ -2,7 +2,16 @@ import Const
 import Graphics.Gloss
 
 window :: Display
-window = InWindow "Snake" (windowWidth*side, windowHeight*side) (0, 0)
+window = InWindow "Snake" (windowWidth, windowHeight) (0, 0)
+
+--Shifting origin to bottom left corner
+translateHeight :: Float
+translateHeight = fromIntegral (side - windowHeight)/2
+
+translateWidth :: Float
+translateWidth = fromIntegral (side - windowWidth)/2
+
+pixel = translate translateWidth translateHeight $ rectangleSolid sideF sideF
 
 main :: IO ()
-main = display window white (rectangleSolid sideF sideF)
+main = display window white pixel
