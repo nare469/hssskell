17b773666475dc5ed11e5154e050114bd65a0ffe
nare469 <nare469@gmail.com>
2015-09-02 00:13:06 -0400

    Restructured the data

--- Main.hs
+++ Main.hs
@@ -4,7 +4,14 @@ import Graphics.Gloss
 --data Definitions
 data Pixel = Pixel Int Int
 
-data Snake = [Pixel]
+translatePixel :: Int -> Int -> Pixel -> Pixel
+translatePixel dx dy (Pixel x y) = Pixel (x+dx) (y+dy)
+
+type Snake = [Pixel]
+
+data Game = Game { snakePos :: Snake }
+initialState :: Game 
+initialState = Game {snakePos = [Pixel (div wBlocks 2) (div hBlocks 2)] }
 
 --Shifting origin to bottom left corner
 translateHeight :: Float
@@ -18,20 +25,16 @@ drawPixelOrigin :: Picture
 drawPixelOrigin = translate translateWidth translateHeight $ rectangleSolid sideF sideF
 
 drawPixel :: Pixel -> Picture
-drawPixel (Pixel x y) = translate (fromIntegral side*x) (fromIntegral side*y) drawPixelOrigin
+drawPixel (Pixel x y) = translate (fromIntegral $ side*x) (fromIntegral $ side*y) drawPixelOrigin
 
 drawSnake :: Snake -> [Picture]
 drawSnake = map drawPixel
 
-translatePixel :: Int -> Int -> Pixel -> Pixel
-translatePixel dx dy (Pixel x y) = Pixel (x+dx) (y+dy)
-
-frame = pictures [
-    translatePixel 1 2 pixel,
-    translatePixel 4 4 pixel ]
-
 window :: Display
 window = InWindow "Snake" (windowWidth, windowHeight) (0, 0)
 
+renderState :: Game -> Picture
+renderState = pictures . drawSnake . snakePos
+
 main :: IO ()
-main = display window white frame
+main = display window white $ renderState initialState
