04f08257a5f5fd94e0396e4e775e9a9cc7bf5a83
nare469 <nare469@gmail.com>
2015-09-01 23:09:12 -0400

    Changed a few names

--- Const.hs
+++ Const.hs
@@ -6,8 +6,14 @@ side = 20
 sideF :: Float
 sideF = fromIntegral side
 
+vBlocks :: Int
+vBlocks = 20
+
+hBlocks :: Int
+hBlocks = 20
+
 windowHeight :: Int
-windowHeight = 20
+windowHeight = vBlocks*side
 
 windowWidth :: Int
-windowWidth = 20
+windowWidth = hBlocks*side
